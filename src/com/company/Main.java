3ec70203579f404963494e934052007f14657fc7
Pau Egea <papau84@gmail.com>
2021-02-22 23:34:34 +0100

    Hola mundo Mi primera prueba de git un poco de musica

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Una modificacion");
         //vamos a ver si hacemos un ultimo comit and push
         System.out.println("Rama");
+        //ya solo me queda la mala notias
     }
 }
